954346ccf1ff44c91abf8f836affcdc956bbaf44
Emilse Arias <emilsearias@gmail.com>
2024-03-04 21:40:00 -0300

    agregamos un ejemplo de setTimeOut y setInterval

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// window.alert('un mensaje nuevo')
+
+//setTimeout
+
+function comenzarJuego(){
+    document.write('<p>Comenzando la partida de LOL... </p>')
+}
+
+// window.setTimeout(comenzarJuego, 5000);
+// setTimeout(()=>{
+//     document.write('<p>Finalizo la partida </p>')
+// },3000);
+
+//setInterval
+setInterval(comenzarJuego, 3000);
