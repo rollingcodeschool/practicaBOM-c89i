12fc2d070a0f5795cfeb5f20e6a313071d3060ff
Emilse Arias <emilsearias@gmail.com>
2024-03-11 19:40:13 -0300

    agregamos un ejemplo de la funcion clearInterval

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@
 //setTimeout
 
 function comenzarJuego(){
-    document.write('<p>Comenzando la partida de LOL... </p>')
+    document.write(`<p>Comenzando la partida de LOL... Num: ${contador}</p>`);
+    //preguntar si ya se ejecuto 6 veces la funcion
+    if(contador === 6){
+        //detener el setInterval
+        return clearInterval(idInterval)
+    }
+    contador++;
 }
 
 // window.setTimeout(comenzarJuego, 5000);
@@ -12,4 +18,6 @@ function comenzarJuego(){
 // },3000);
 
 //setInterval
-setInterval(comenzarJuego, 3000);
+const idInterval = setInterval(comenzarJuego, 3000);
+console.log(idInterval)
+let contador = 1;
